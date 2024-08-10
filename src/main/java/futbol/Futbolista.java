6fdb0682ed209bc62c79cf2b46d36b40bff45f6c
jerozapata <159493286+jerozapata@users.noreply.github.com>
2024-08-10 17:45:07 -0500

    toma 2

--- src/main/java/futbol/Futbolista.java
+++ src/main/java/futbol/Futbolista.java
@@ -1,5 +1,64 @@
 package futbol;
 
-public abstract class Futbolista {
+public abstract class Futbolista implements Comparable{
+
+    //Atributos privados
+    private String nombre;
+    private int edad;
+    private final String posicion;
+
+    // Constructor para dar valor a los atributos
+    public Futbolista(String nombre, int edad, String posicion){
+
+        this.nombre=nombre;
+        this.edad=edad;
+        this.posicion=posicion;
+    }
+
+    // Constructor por defecto
+    public Futbolista(){
+        this("Maradona", 30, "delantero");
+    }
+
+    @Override
+    public String toString(){
+
+        return "El futbolista "+ nombre + " tiene "+ edad + " años, y juega de "+ posicion+".";
+        
+    }
+
+    public boolean equals(Futbolista f){
+        return this.nombre.equals(f.nombre) && this.edad == f.edad && this.posicion.equals(f.posicion);
+    }
+
+    public abstract boolean juagarConLasManos();
+
+    // Setter y getters
+
+    public String getNombre(){
+
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+
+        this.nombre=nombre;
+
+    }
+
+    public int getEdad(){
+
+        return edad;
+    }
+
+    public void setEdad(int edad){
+        this.edad=edad;
+    }
+
+    public String getPosicion(){
+        return posicion;
+    }
+
+    
 
 }
