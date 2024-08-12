55b947c0e97f684133d5140c85938ac7b985444b
jerozapata <159493286+jerozapata@users.noreply.github.com>
2024-08-12 17:50:55 -0500

    Toma 20

---
+++ src/main/java/futbol/Jugador.java
@@ -0,0 +1,39 @@
+package futbol;
+
+public class Jugador extends Futbolista{
+
+    public short golesMarcados;
+    public byte dorsal;
+
+    public Jugador(){
+
+        super();
+        this.golesMarcados = 289;
+        this.dorsal = 7;
+    }
+
+    public Jugador(String nombre, int edad, String posicion, short golesMarcados, byte dorsal){
+
+        super(nombre, edad, posicion);
+        this.golesMarcados = golesMarcados;
+        this.dorsal = dorsal;
+    }
+
+    @Override
+    public boolean jugarConLasManos() {
+        return false;
+    }
+
+    
+    public int compareTo(Futbolista f){
+
+        return Math.abs(this.getEdad() - f.getEdad());
+    }
+
+    @Override
+    public String toString(){
+
+        return "El futbolista " + getNombre() + " tiene " + getEdad() + ", y juega de " + getPosicion() + " con el dorsal " + dorsal + ". Ha marcado " + golesMarcados;   
+    }
+
+}
